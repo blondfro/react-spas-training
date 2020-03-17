f83e667abaf8b3c1d583e68a71e816b15ad6ff6a
James Morris <blondfro@gmail.com>
2020-03-16 21:21:04 -0500

    created a navigation and added in an icon.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Home from "./components/Home";
 import Welcome from "./components/Welcome";
+import Navigation from "./components/Navigation";
 
 function App() {
-  const [user, setUser] = useState("Ray");
+  const [user, setUser] = useState("James");
 
   return (
     <div>
+      <Navigation user={user} />
       {user && <Welcome user={user} />}
       <Home user={user} />
     </div>
